6906e720bca4d7b7ecdaa83f2027122c06c1b4df
P3E009-E <P3E009-E@39681614D20P3>
2025-05-16 10:33:10 -0500

    Added Buttom Login Activity

--- app/src/main/java/com/damarys/ona/cazarpatos/LoginActivity.kt
+++ app/src/main/java/com/damarys/ona/cazarpatos/LoginActivity.kt
@@ -1,21 +1,30 @@
 package com.damarys.ona.cazarpatos
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
+private lateinit var buttonLogin : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+        buttonLogin = findViewById(R.id.buttonLogin)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        buttonLogin.setOnClickListener {
+            // Cuando se hace clic, se inicia la MainActivity
+            val intent = Intent(this@LoginActivity, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
