0bfd0d57aeeae9b0c77150b00e7d3f90b6868404
P3E009-E <P3E009-E@39681614D20P3>
2025-07-11 10:51:51 -0500

    Added RankingPlayerDBHelper, and modified RankingActivity.

--- app/src/main/java/com/damarys/ona/cazarpatos/RankingActivity.kt
+++ app/src/main/java/com/damarys/ona/cazarpatos/RankingActivity.kt
@@ -7,18 +7,20 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.damarys.ona.cazarpatos.database.RankingPlayerDBHelper
 
 class RankingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_ranking)
+
         /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }*/
-        var jugadores = arrayListOf<Player>()
+        /*var jugadores = arrayListOf<Player>()
         jugadores.add(Player("Damarys.Oña",10))
         jugadores.add(Player("Jugador2",6))
         jugadores.add(Player("Jugador3",3))
@@ -28,7 +30,42 @@ class RankingActivity : AppCompatActivity() {
         val recyclerViewRanking: RecyclerView = findViewById(R.id.recyclerViewRanking);
         recyclerViewRanking.layoutManager = LinearLayoutManager(this);
         recyclerViewRanking.adapter = RankingAdapter(jugadores);
-        recyclerViewRanking.setHasFixedSize(true);
+        recyclerViewRanking.setHasFixedSize(true);*/
 
+        OperacionesSqLite()
+        GrabarRankingSQLite()
+        LeerRankingsSQLite()
     }
+    fun OperacionesSqLite() {
+        RankingPlayerDBHelper(this).deleteAllRanking()
+        RankingPlayerDBHelper(this).insertRankingByQuery(Player("Jugador9", 10))
+        val patosCazados = RankingPlayerDBHelper(this).readDucksHuntedByPlayer("Jugador9")
+        RankingPlayerDBHelper(this).updateRanking(Player("Jugador9", 5))
+        RankingPlayerDBHelper(this).deleteRanking("Jugador9")
+        RankingPlayerDBHelper(this).insertRanking(Player("Jugador9", 7))
+        val players = RankingPlayerDBHelper(this).readAllRankingByQuery()
+    }
+
+    fun GrabarRankingSQLite() {
+        val jugadores = arrayListOf(
+            Player("Damarys.Oña", 10),
+            Player("Jugador2", 6),
+            Player("Jugador3", 3),
+            Player("Jugador4", 9)
+        )
+        jugadores.sortByDescending { it.huntedDucks }
+        val dbHelper = RankingPlayerDBHelper(this)
+        for (jugador in jugadores) {
+            dbHelper.insertRanking(jugador)
+        }
+    }
+
+    fun LeerRankingsSQLite() {
+        val jugadoresSQLite = RankingPlayerDBHelper(this).readAllRanking()
+        val recyclerViewRanking: RecyclerView = findViewById(R.id.recyclerViewRanking)
+        recyclerViewRanking.layoutManager = LinearLayoutManager(this)
+        recyclerViewRanking.adapter = RankingAdapter(jugadoresSQLite)
+        recyclerViewRanking.setHasFixedSize(true)
+    }
+
 }
